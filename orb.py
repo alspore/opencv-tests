e53192806f66c94928aca337b88eabb94e6b6748
Aaron Landis <alspore@gmail.com>
2013-10-13 23:07:28 -0400

    initial commit
    added...
        orb.py
        trackbar.py
        webcam.py

---
+++ orb.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+orb = cv2.ORB()
+
+while True:
+    ret, frame = cap.read()
+
+    kp = orb.detect(frame, None)
+
+    img = cv2.drawKeypoints(frame, kp, color=(0, 255, 0), flags=0)
+
+    cv2.imshow('frame', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
